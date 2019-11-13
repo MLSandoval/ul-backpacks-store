bd0ab11a0b124f706a6476d1c0a912ddcb8f314d
michael sandoval <michaelsandoval8@gmail.com>
2019-11-13 21:16:44 +0000

    base environment arranged

---
+++ client/components/app.jsx
@@ -0,0 +1,19 @@
+import react from 'react';
+//import all other components used in the app parent component
+
+export default class App extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+
+        },
+    }
+}
+
+render(){
+
+    return(
+        
+    );
+}
