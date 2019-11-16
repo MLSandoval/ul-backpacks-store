36e52329fd6c061a33a5c704ff0afa881fb6c42b
michael sandoval <michaelsandoval8@gmail.com>
2019-11-16 07:00:35 +0000

    last commit for day

--- client/components/landing.jsx
+++ client/components/landing.jsx
@@ -4,10 +4,21 @@ import './styles/landing_style.css';
 export default function Landing (props){
     return (
       <React.Fragment>
-        <div className="position-fixed landing-header navbar navbar-default navbar-fixed-top">
+        <div className="position-fixed landing-header navbar">
           <div className="container-fluid">
             <div className="row">
-              <div className="col-12">UltraLite</div>
+              <div className="col-12 row">UltraLite
+                <div className="products col-4 dropdown">
+                    <button className="dropdown-toggle" 
+                    type="button"
+                    data-toggle="dropdown">
+                        Products
+                    </button>
+                    <div className="dropdown-menu">
+                        <a href="www.google.com" className="dropdown-item">Backpacking Bags</a>
+                    </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -26,14 +37,7 @@ export default function Landing (props){
               Est sit amet facilisis magna etiam tempor orci eu. Lacus sed
               viverra tellus in hac habitasse platea dictumst vestibulum. Rutrum
               tellus pellentesque eu tincidunt tortor aliquam. Porta nibh
-              venenatis cras sed felis eget velit. Amet mattis vulputate enim
-              nulla aliquet porttitor lacus luctus accumsan. Morbi tincidunt
-              augue interdum velit euismod in pellentesque massa placerat.
-              Convallis a cras semper auctor neque vitae tempus quam. Dictumst
-              quisque sagittis purus sit amet volutpat consequat. Nisi quis
-              eleifend quam adipiscing vitae proin sagittis nisl. Habitasse
-              platea dictumst vestibulum rhoncus est pellentesque elit
-              ullamcorper. Ultrices neque ornare aenean euismod elementum nisi.
+              venenatis cras sed felis eget velit. 
             </div>
             <div className="bg bg-2 col-12">BG-2</div>
             <div className="text text-2 col-12">
@@ -65,6 +69,26 @@ export default function Landing (props){
     );
 }
 
-<nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container">...</div>
-</nav>;
+<div class="dropdown">
+  <button
+    class="btn btn-secondary dropdown-toggle"
+    type="button"
+    id="dropdownMenuButton"
+    data-toggle="dropdown"
+    aria-haspopup="true"
+    aria-expanded="false"
+  >
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">
+      Action
+    </a>
+    <a class="dropdown-item" href="#">
+      Another action
+    </a>
+    <a class="dropdown-item" href="#">
+      Something else here
+    </a>
+  </div>
+</div>;
