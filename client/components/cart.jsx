8534d9c8086ac05e6621792a9f99f12f640e959e
michael sandoval <michaelsandoval8@gmail.com>
2019-12-10 06:56:21 +0000

    created custom eslint file for this project

--- client/components/cart.jsx
+++ client/components/cart.jsx
@@ -3,12 +3,9 @@ import {connect} from 'react-redux';
 
 import './styles/cart_style.css';
 
-class Cart extends React.Component{
-
-
-    render(){
-
-        return(
+class Cart extends React.Component {
+  render () {
+     return (
             <div className="pt-4">
                 <h1 className="pt-4">THIS IS THE CART VIEW</h1>
             </div>
