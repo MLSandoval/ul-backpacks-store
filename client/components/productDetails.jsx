948259d3e75bfe80ff09f6c6a720612c84c40a08
mlsandoval <michaelsandoval8@gmail.com>
2020-01-08 23:55:13 +0000

    still working on image carousel

--- client/components/productDetails.jsx
+++ client/components/productDetails.jsx
@@ -19,34 +19,34 @@ class ProductDetails extends React.Component {
             </a>
           </div>
 
-          <img className="col-8 " src={'../' + this.props.products[id].images[0]} alt="" />
+          {/* <img className="col-8 " src={'../' + this.props.products[id].images[0]} alt="" /> */}
 
-          <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel" data-interval="false">
+          <div id="images" className="col-8 carousel slide" data-ride="carousel" data-interval="false">
             <ol className="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+              <li data-target="#images" data-slide-to="0" className="active"></li>
+              <li data-target="#images" data-slide-to="1"></li>
+              <li data-target="#images" data-slide-to="2"></li>
+              <li data-target="#images" data-slide-to="3"></li>
             </ol>
               <div className="carousel-inner">
                 <div className="carousel-item active">
-                  <img className="d-block w-100" src={this.props.products[id].images[0]} alt="First slide"/>
+                  <img className="d-block w-100" src={'../' + this.props.products[id].images[0]} alt="First slide"/>
                 </div>
                 <div className="carousel-item">
-                  <img className="d-block w-100" src={this.props.products[id].images[1]} alt="Second slide"/>
+                  <img className="d-block w-100" src={'../' + this.props.products[id].images[1]} alt="Second slide"/>
                 </div>
                 <div className="carousel-item">
-                  <img className="d-block w-100" src={this.props.products[id].images[2]} alt="Third slide"/>
+                  <img className="d-block w-100" src={'../' + this.props.products[id].images[2]} alt="Third slide"/>
                 </div>
                 <div className="carousel-item">
-                  <img className="d-block w-100" src={this.props.products[id].images[3]} alt="Third slide"/>
+                  <img className="d-block w-100" src={'../' + this.props.products[id].images[3]} alt="Fourth slide"/>
                 </div>
             </div>
-            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <a className="carousel-control-prev" href="#images" role="button" data-slide="prev">
               <span className="carousel-control-prev-icon" aria-hidden="true"></span>
               <span className="sr-only">Previous</span>
             </a>
-            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <a className="carousel-control-next" href="#images" role="button" data-slide="next">
               <span className="carousel-control-next-icon" aria-hidden="true"></span>
               <span className="sr-only">Next</span>
             </a>
