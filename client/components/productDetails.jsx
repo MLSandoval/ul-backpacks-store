3ae942a7ad24c17cc61214785f2a76e6a9001089
mlsandoval <michaelsandoval8@gmail.com>
2020-01-09 03:18:45 +0000

    made asset size of dummy data images uniform, will have to do for full product set. Image carousel working smoothly, will have to format and css later

--- client/components/productDetails.jsx
+++ client/components/productDetails.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import Carousel from 'react-bootstrap/Carousel'
+
+import "./styles/product_details_style.css"
+
 class ProductDetails extends React.Component {
   render () {
     console.log('this.props in product details: ', this.props);
@@ -20,37 +24,57 @@ class ProductDetails extends React.Component {
           </div>
 
           {/* <img className="col-8 " src={'../' + this.props.products[id].images[0]} alt="" /> */}
+          <div className="col-8 carousel-container">
+            <Carousel interval={false}>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={'../' + this.props.products[id].images[0]}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={'../' + this.props.products[id].images[1]}
+                  alt="Third slide"
+                />
 
-          <div id="images" className="col-8 carousel slide" data-ride="carousel" data-interval="false">
-            <ol className="carousel-indicators">
-              <li data-target="#images" data-slide-to="0" className="active"></li>
-              <li data-target="#images" data-slide-to="1"></li>
-              <li data-target="#images" data-slide-to="2"></li>
-              <li data-target="#images" data-slide-to="3"></li>
-            </ol>
-              <div className="carousel-inner">
-                <div className="carousel-item active">
-                  <img className="d-block w-100" src={'../' + this.props.products[id].images[0]} alt="First slide"/>
-                </div>
-                <div className="carousel-item">
-                  <img className="d-block w-100" src={'../' + this.props.products[id].images[1]} alt="Second slide"/>
-                </div>
-                <div className="carousel-item">
-                  <img className="d-block w-100" src={'../' + this.props.products[id].images[2]} alt="Third slide"/>
-                </div>
-                <div className="carousel-item">
-                  <img className="d-block w-100" src={'../' + this.props.products[id].images[3]} alt="Fourth slide"/>
-                </div>
-            </div>
-            <a className="carousel-control-prev" href="#images" role="button" data-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="sr-only">Previous</span>
-            </a>
-            <a className="carousel-control-next" href="#images" role="button" data-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="sr-only">Next</span>
-            </a>
-        </div>
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={'../' + this.props.products[id].images[2]}
+                  alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={'../' + this.props.products[id].images[3]}
+                  alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
           
 
 
