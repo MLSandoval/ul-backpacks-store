eeba03a1cda2e7743c1de2550dea443026d8cc81
mlsandoval <michaelsandoval8@gmail.com>
2020-05-17 07:20:16 +0000

    fixed cart component with new cart, abandoned making cart rows link back to details page

--- client/components/cart.jsx
+++ client/components/cart.jsx
@@ -183,14 +183,14 @@ class Cart extends React.Component {
 
   componentDidMount(){
     
-    this.props.computeCartTotal(this.props.cart.cart_items)
+    this.props.computeCartTotal(this.props.cart.cart_items, this.props.products)
     // console.log('Cart component props: ', this.props)
     // this.props.computeCartTotal(this.props.cart)
     // console.log('cart didmount compute Cart total: ', this.props.totalOrderCost)
   }
   componentDidUpdate(){
     // console.log('cart DidUpdate, this.props.cart: ', this.props.cart)
-    this.props.computeCartTotal(this.props.cart)
+    this.props.computeCartTotal(this.props.cart.cart_items, this.props.products)
   }
 
   render () {
