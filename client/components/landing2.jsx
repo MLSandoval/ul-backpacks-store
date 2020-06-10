6e2374f3488e90e436fe86bb39a0403c3f155ee9
mlsandoval <michaelsandoval8@gmail.com>
2020-06-10 10:23:46 +0000

    landing styling for mobile reworked, removed 2 cards for product sets, repositioned and resized images and cards ontop of images.

--- client/components/landing2.jsx
+++ client/components/landing2.jsx
@@ -54,52 +54,44 @@ class Landing extends React.Component {
             <div></div>
           </div>
             <div className=" text text-2 col-12 d-flex align-items-center justify-content-end flex-column">
-              <h1>Our gear can make it happen</h1>
+              <h1>Let our gear make it happen</h1>
             </div>
           <div className="col-12 hori-break"></div>
-          <img className=" img-2" src="../../images/gear_sets/gear_set_up_4.jpg" alt=""/> 
-          <CardDeck className="product-cards pb-3 d-flex flex-wrap justify-content-around">
-            <FadeInSection classesPassed="col-9 p-3">
-              <Card as={LinkRouter} to="/products" className="col-12 align-items-center">
-                <Card.Img className="card-img pt-2" variant="top" src="../../images/logos_icons/backpack_icon.png" />
-                <Card.Body>
-                  <Card.Title></Card.Title>
-                  <Card.Text>Backpacks</Card.Text>
-                </Card.Body>
-                <Card.Footer></Card.Footer>
-              </Card>
-            </FadeInSection>
-            <FadeInSection classesPassed="col-9 p-3">
-              <Card className="col-12 align-items-center">
-                <Card.Img className="card-img pt-2" variant="top" src="../../images/logos_icons/tent_icon.png" />
-                <Card.Body>
-                  <Card.Title></Card.Title>
-                  <Card.Text>Tents</Card.Text>
-                </Card.Body>
-                <Card.Footer className="text-muted text-sm">Coming Soon</Card.Footer>
-              </Card>
-            </FadeInSection>
-            <FadeInSection classesPassed="col-9 p-3">
-              <Card className="col-12 align-items-center">
-                <Card.Img className="card-img pt-2" variant="top" src="../../images/logos_icons/sleeping_bag_icon.png" />
-                <Card.Body>
-                  <Card.Title></Card.Title>
-                  <Card.Text>Sleeping Bags</Card.Text>
-                </Card.Body>
-                <Card.Footer className="text-muted text-sm">Coming Soon</Card.Footer>
-              </Card>
-            </FadeInSection>
-            <FadeInSection classesPassed="col-9 p-3">
-              <Card className="col-12 align-items-center">
-                <Card.Img className="card-img pt-2" variant="top" src="../../images/logos_icons/outerwear_icon.png" />
-                <Card.Body>
-                  <Card.Title></Card.Title>
-                  <Card.Text>Clothing</Card.Text>
-                </Card.Body>
-                <Card.Footer className="text-muted text-sm">Coming Soon</Card.Footer>
-              </Card>
-            </FadeInSection>
-          </CardDeck>
+          <div className="d-flex flex-column position-relative">
+          <img className="img-2" src="../../images/gear_sets/gear_set_up_4.jpg" alt=""/> 
+            <CardDeck className="product-cards pb-3 d-flex flex-wrap justify-content-around">
+              <FadeInSection classesPassed="col-9 p-3">
+                <Card as={LinkRouter} to="/products" className="col-12 align-items-center">
+                  <Card.Img className="card-img pt-2" variant="top" src="../../images/logos_icons/backpack_icon.png" />
+                  <Card.Body>
+                    <Card.Title></Card.Title>
+                    <Card.Text>Backpacks</Card.Text>
+                  </Card.Body>
+                  <Card.Footer></Card.Footer>
+                </Card>
+              </FadeInSection>
+              <FadeInSection classesPassed="col-9 p-3">
+                <Card className="col-12 align-items-center">
+                  <Card.Img className="card-img pt-2" variant="top" src="../../images/logos_icons/tent_icon.png" />
+                  <Card.Body>
+                    <Card.Title></Card.Title>
+                    <Card.Text>Tents</Card.Text>
+                  </Card.Body>
+                  <Card.Footer className="text-muted text-sm">Coming Soon</Card.Footer>
+                </Card>
+              </FadeInSection>
+              <FadeInSection classesPassed="col-9 p-3">
+                <Card className="col-12 align-items-center">
+                  <Card.Img className="card-img pt-2" variant="top" src="../../images/logos_icons/sleeping_bag_icon.png" />
+                  <Card.Body>
+                    <Card.Title></Card.Title>
+                    <Card.Text>Sleeping Bags</Card.Text>
+                  </Card.Body>
+                  <Card.Footer className="text-muted text-sm">Coming Soon</Card.Footer>
+                </Card>
+              </FadeInSection>
+            </CardDeck>
+          </div>
         </div>
       </div>
     )
